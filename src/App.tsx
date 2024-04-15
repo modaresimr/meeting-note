0e016a3dc329087cf36758af5372cc0f65e16c71
modaresimr <9498182+modaresimr@users.noreply.github.com>
2024-04-15 09:38:54 +0000

    update

--- src/App.tsx
+++ src/App.tsx
@@ -9,11 +9,13 @@ function App() {
         <div className='flex justify-center items-center min-h-screen'>
             <div className='container flex flex-col justify-center items-center'>
                 <h1 className='text-5xl font-extrabold tracking-tight text-slate-900 sm:text-7xl text-center'>
-                    Whisper Web
+                    Meeting Note
                 </h1>
+                
                 <h2 className='mt-3 mb-5 px-4 text-center text-1xl font-semibold tracking-tight text-slate-900 sm:text-2xl'>
-                    ML-powered speech recognition directly in your browser
+                    Offline speech recognition. No data send to any server.
                 </h2>
+                
                 <AudioManager transcriber={transcriber} />
                 <Transcript transcribedData={transcriber.output} />
                 <div className='bottom-4'>
@@ -23,7 +25,7 @@ function App() {
                     href='https://github.com/xenova/transformers.js'
                 >
                     🤗 Transformers.js
-                </a>
+                </a>. Forked from <a href="https://github.com/xenova/whisper-web">Whisper Web</a>. By <a href="https://github.com/modaresimr/">Ali Modaresi</a>
             </div>
             </div>
 
